8cb29788259df332a2e4bfaa38bf99b2af5320ef
Sachin <mailtosachin28@gmail.com>
2022-11-11 23:09:37 +0530

    first commit

---
+++ src/test/java/dataDriven/excelDataProvider/dataProvide.java
@@ -0,0 +1,54 @@
+package dataDriven.excelDataProvider;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class dataProvide {
+	// multiple sets of data to our tests
+	// Array public
+	// 5 sets of data as 5 arrays from data provider to your test
+	// then our test will run 5 times with 5 separate sets of data(array)
+	DataFormatter formatter = new DataFormatter();
+
+	@Test(dataProvider = "driveTest")
+	public void testCaseData(String greeting, String communication, String id) {
+		System.out.println(greeting + communication + id);
+
+	}
+
+	@DataProvider(name = "driveTest")
+	public Object[][] getData() throws IOException {
+		/*
+		 * Object[][] data = { { "hello", "Text", 1 }, { "bye", "Message", 123 }, {
+		 * "solo", "call", 456 } }; return data;
+		 */
+		FileInputStream fis = new FileInputStream("C:\\Users\\sachi\\Documents\\excelDriven.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheetAt(0);
+		int rowCount = sheet.getPhysicalNumberOfRows();
+		XSSFRow row = sheet.getRow(0);
+		int colcount = row.getLastCellNum();
+		Object data[][] = new Object[rowCount - 1][colcount];
+		for (int i = 0; i<rowCount-1; i++) {
+			row = sheet.getRow(i+1);
+			for (int j = 0; j <colcount; j++) {
+				XSSFCell cell = row.getCell(j);
+
+				data[i][j] = formatter.formatCellValue(cell);
+
+			}
+		}
+		return data;
+
+	}
+
+}
